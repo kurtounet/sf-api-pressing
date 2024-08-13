d91e684254ebe1dce7cab2b666d8307015962259
kurtounet <bossut.anthony@gmail.com>
2024-08-13 12:52:14 +0200

    update

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
