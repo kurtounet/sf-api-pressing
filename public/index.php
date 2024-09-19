528782f04b84dd389af4322d4f6f5184cae2e561
kurtounet <bossut.anthony@gmail.com>
2024-09-19 11:55:26 +0200

    delete cors public/index

--- public/index.php
+++ public/index.php
@@ -2,19 +2,19 @@
 
 use App\Kernel;
 
-header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: *");
-header("Access-Control-Allow-Methods: *");
-header("Allow: *");
+// header('Access-Control-Allow-Origin: *');
+// header("Access-Control-Allow-Headers: *");
+// header("Access-Control-Allow-Methods: *");
+// header("Allow: *");
 
-$method = $_SERVER['REQUEST_METHOD'];
+// $method = $_SERVER['REQUEST_METHOD'];
 
-if ($method === "OPTIONS") {
-    die();
-}
+// if ($method === "OPTIONS") {
+//     die();
+// }
 
 require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool)$context['APP_DEBUG']);
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
