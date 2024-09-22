e74e00048b4d53f5b537a07d6e14e32da02fbf0b
kurtounet <bossut.anthony@gmail.com>
2024-09-22 09:47:38 +0200

    update

--- src/Entity/Employee.php
+++ src/Entity/Employee.php
@@ -8,7 +8,11 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Attribute\Groups;
-
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
 
 #[ApiResource(
     normalizationContext: ['groups' => ['employee:read']],
