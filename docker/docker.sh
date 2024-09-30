8482f36be5679fb2c3a36e399d67a92e0ece4a96
kurtounet <bossut.anthony@gmail.com>
2024-09-30 13:00:47 +0200

    update

---
+++ docker/docker.sh
@@ -0,0 +1,5 @@
+# php bin/console d:d:c --no-interaction
+# php bin/console d:m:m --no-interaction
+# php bin/console d:f:l --no-interaction
+# #php bin/console app:create-user admin@gmail.com admin
+# exec apache2-foreground
