7b2d33f06e359ea42750362cf1524d0caa8e58d7
Kurtounet <80813899+kurtounet@users.noreply.github.com>
2024-11-21 15:25:58 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -2,16 +2,7 @@
 
 use App\Kernel;
 
-// header('Access-Control-Allow-Origin: *');
-// header("Access-Control-Allow-Headers: *");
-// header("Access-Control-Allow-Methods: *");
-// header("Allow: *");
 
-// $method = $_SERVER['REQUEST_METHOD'];
-
-// if ($method === "OPTIONS") {
-//     die();
-// }
 
 require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
